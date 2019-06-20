04c4b315aa874ed348510dc6f57f3fceb8482c3e
Nyiramirembe <peninahhot@gmail.com>
2019-06-20 00:03:48 -0700

    hello-contacts

---
+++ Literal Notation.js
@@ -0,0 +1,17 @@
+function Dog(name, colors,age){
+this.name= name;
+this.colors = colors;
+this.age = age;
+}
+var falcor = new Dog("Falcor" ,["black"],4);
+var nola = new Dog("Nola",["white","black"],6);
+var patsy = new Dog("patsy",["brown"],7);
+Dog.class_name.prototype.method_name = function(first_argument) {
+	// body...
+
+console.log("woof");
+
+Dog.class_name.prototype.method_name = function(first_argument) {
+	// body...
+};
+
